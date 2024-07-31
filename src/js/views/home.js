b29d12f0c8a13fb72adaf3fe1d18dcfcd90e39b7
4lvaro8 <ponitsa@gmail.com>
2024-07-31 19:22:04 +0000

    mayorParte

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="main container-fluid">
-		<div className="button-container ">
-			<button className="btn bg-success text-white float-end p-2 me-3 mt-3">Add new contact</button>
-		</div>
-		<div className="contact-container">
-			<div className="row">
-				<div className="col-12"></div>
-			</div>
-		</div>		
+import Contacts from "../component/contacts";
+
+function Home() {
+	return(
+	<>
+	<div className="py-3 d-flex justify-content-end">
+		<Link to="/NewContact" className="btn btn-primary" type="button">
+			Add new contact
+		</Link>
 	</div>
-);
+	<div className="">
+		<ul className="list-group">
+			<Contacts />
+		</ul>
+	</div>
+	</>
+	)
+};
+
+export default Home;
